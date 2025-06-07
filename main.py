89def306b8acea68fa019767aabe04efc336e057
Apostolos Chalis <tolishalis@gmail.com>
2025-06-07 16:17:45 +0300

    /links route added

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import httpx
 from bs4 import BeautifulSoup
 
 app = FastAPI()
+url = "https://news.ycombinator.com/"
 
-@app.get("/")
-async def scrape():
-    url = "https://news.ycombinator.com/"
+@app.get("/titles")
+async def titles():
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
     
@@ -14,4 +14,16 @@ async def scrape():
     titles = soup.find_all("td", class_="title")
 
     return {"titles": [title.get_text(strip=True) for title in titles]}
+
+@app.get("/links")
+async def links(): 
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    links = soup.find_all("span", class_="sitestr")
+
+    return {"links": [link.get_text(strip=True) for link in links]}
+    
+
 
