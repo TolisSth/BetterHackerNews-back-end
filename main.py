ba8dcf13e903c89ab07fc5d50598403561775cff
Apostolos Chalis <tolishalis@gmail.com>
2025-06-07 16:58:02 +0300

    Fixed problems in /title route

--- main.py
+++ main.py
@@ -7,13 +7,17 @@ url = "https://news.ycombinator.com/"
 
 @app.get("/titles")
 async def titles():
+    url = "https://news.ycombinator.com/"  # Make sure 'url' is defined
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
-    
-    soup = BeautifulSoup(response.text, "html.parser")
-    titles = soup.find_all("td", class_="title")
 
-    return {"titles": [title.get_text(strip=True) for title in titles]}
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Find all the title links
+    titles = soup.find_all("td", class_="title")
+    all_titles = [title.get_text(strip=True) for i, title in enumerate(titles) if i % 2 != 0]
+
+    return {"odd_titles": all_titles}
 
 @app.get("/links")
 async def links(): 
@@ -21,9 +25,8 @@ async def links():
         response = await client.get(url)
 
     soup = BeautifulSoup(response.text, "html.parser")
+    # Hacker News uses <span> tags instead of <a> tags for links
     links = soup.find_all("span", class_="sitestr")
 
     return {"links": [link.get_text(strip=True) for link in links]}
     
-
-
