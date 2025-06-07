79b84483b8bf314a10ff7fe045d3f9ece261b302
Apostolos Chalis <tolishalis@gmail.com>
2025-06-07 12:28:45 +0300

    initial

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import httpx
+from bs4 import BeautifulSoup
+
+app = FastAPI()
+
+@app.get("/")
+async def scrape():
+    url = "https://news.ycombinator.com/"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+    
+    soup = BeautifulSoup(response.text, "html.parser")
+    # TODO: eliminate the numbers, for now I will handle it on the front end
+    titles = soup.find_all("td", class_="title")
+
+    return {"titles": [title.get_text(strip=True) for title in titles]}
+
