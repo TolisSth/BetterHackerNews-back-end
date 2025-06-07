55682f56575dbdf6a101b8d9c3a120fdd19d5287
Apostolos Chalis <tolishalis@gmail.com>
2025-06-07 20:11:14 +0300

    CORS enablement

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
 import httpx
+from fastapi.middleware.cors import CORSMiddleware
 from bs4 import BeautifulSoup
 
 app = FastAPI()
 url = "https://news.ycombinator.com/"
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/titles")
 async def titles():
-    url = "https://news.ycombinator.com/"  # Make sure 'url' is defined
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
 
@@ -17,7 +26,7 @@ async def titles():
     titles = soup.find_all("td", class_="title")
     all_titles = [title.get_text(strip=True) for i, title in enumerate(titles) if i % 2 != 0]
 
-    return {"odd_titles": all_titles}
+    return {"titles": all_titles}
 
 @app.get("/links")
 async def links(): 
@@ -26,7 +35,7 @@ async def links():
 
     soup = BeautifulSoup(response.text, "html.parser")
     # Hacker News uses <span> tags instead of <a> tags for links
-    links = soup.find_all("span", class_="sitestr")
+    links = soup.find_all("a", class_="span.titleline > a")
 
     return {"links": [link.get_text(strip=True) for link in links]}
     
